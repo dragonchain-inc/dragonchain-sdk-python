4f69e40f9801c226c902c0c6e8b22ad85abd006e
Alex Benedetto <alex061994@gmail.com>
2019-01-24 13:26:40 -0800

    bumped version (#17)

--- dragonchain_sdk/__init__.py
+++ dragonchain_sdk/__init__.py
@@ -13,7 +13,7 @@ from dragonchain_sdk.dragonchain_client import Client
 import logging
 
 __author__ = 'Dragonchain'
-__version__ = '1.0.3'
+__version__ = '1.0.4'
 
 
 def set_stream_logger(name='dragonchain_sdk', level=logging.DEBUG, format_string=None):
